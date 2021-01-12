4b4daf54d4c1eae877a97ea160c85141fd0c1c54
vishwajeetdpatil <vishwajeetmailbox@gmail.com>
2021-01-12 07:23:47 +0000

    pcf 1 lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String message;
+
+    public WelcomeController(@Value("${welcome.message}") String a_welcome_message) {
+        this.message = a_welcome_message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return this.message;
     }
 }
